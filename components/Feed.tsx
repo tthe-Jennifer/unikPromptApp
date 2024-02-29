65d512de1dbc094037fd9b2012d83e8baf933aad
tthe-Jennifer <105241678+tthe-Jennifer@users.noreply.github.com>
2024-02-29 15:23:17 +0200

    fixed issues

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -6,7 +6,7 @@ import PromptCard from './PromptCard';
 const PromptCardList = ({ data, handleTagClick }: any) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((post) => (
+      {data.map((post: any) => (
       <PromptCard 
       key={post._id}
       post={post}
