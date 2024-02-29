e8bbea3ac5c0500d0e02a3e6adbc6bf1609ff685
tthe-Jennifer <105241678+tthe-Jennifer@users.noreply.github.com>
2024-02-29 13:56:30 +0200

    fixed issues

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,41 +1,32 @@
-"use client";
-import { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react"
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import User from "@models/user";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-
-import { connectToDb } from "@utils/database";
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID! as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
-    }),
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    })
   ],
   callbacks: {
-    async session({ session }: { session?: Session }) {
-
-      const actualSession = session ?? { user: { email: '' } };
-
-      const sessionUser = await User.findOne({
-        email: actualSession.user.email,
-      });
-
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
 
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
-        await connectToDb();
-        //check if user already exists
-        const userExists = await User.findOne({
-          email: profile.email,
-        });
-        //if not create new user and save
+        await connectToDB();
+
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
@@ -43,12 +34,69 @@ const handler = NextAuth({
             image: profile.picture,
           });
         }
-        return true;
+
+        return true
       } catch (error) {
-        return false;
+        console.log("Error checking if user exists: ", error.message);
+        return false
       }
     },
-  },
-});
+  }
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
+
+
+
+// -------------------------------------
+// "use client";
+// import { Session } from "next-auth";
+// import { SessionProvider } from "next-auth/react"
+// import NextAuth from "next-auth/next";
+// import GoogleProvider from "next-auth/providers/google";
+// import User from "@models/user";
+
+
+// import { connectToDb } from "@utils/database";
+
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID! as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
+//     }),
+//   ],
+//   callbacks: {
+//     async session({ session }: { session?: Session }) {
+//       const sessionUser = await User.findOne({
+//         email: session.user.email,
+//       });
+
+//       session.user.id = sessionUser._id.toString();
+
+//       return session;
+//     },
+//     async signIn({ profile }) {
+//       try {
+//         await connectToDb();
+//         //check if user already exists
+//         const userExists = await User.findOne({
+//           email: profile.email,
+//         });
+//         //if not create new user and save
+//         if (!userExists) {
+//           await User.create({
+//             email: profile.email,
+//             username: profile.name.replace(/\s/g, "").toLowerCase(),
+//             image: profile.picture,
+//           });
+//         }
+//         return true;
+//       } catch (error) {
+//         return false;
+//       }
+//     },
+//   },
+// });
+
+// export { handler as GET, handler as POST };
