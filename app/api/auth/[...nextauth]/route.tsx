943d49010bc92fb0baa3bb133da8f97cb16d5430
tthe-Jennifer <105241678+tthe-Jennifer@users.noreply.github.com>
2024-02-29 13:05:07 +0200

    fixed issues

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -14,7 +14,7 @@ const handler = NextAuth({
     }),
   ],
   callbacks: {
-    async session({ session }: { session?: Session }) {
+    async session({ session }: { session?: session }) {
       const sessionUser = await User.findOne({
         email: session.user.email,
       });
