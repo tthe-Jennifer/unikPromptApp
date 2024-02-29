5eb68526371f5daac5b7f2bed3c0eb079c67c4d3
tthe-Jennifer <105241678+tthe-Jennifer@users.noreply.github.com>
2024-02-29 15:21:37 +0200

    fixed issues

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 import PromptCard from './PromptCard';
 
-const PromptCardList = ({ data, handleTagClick }) => {
+const PromptCardList = ({ data, handleTagClick }: any) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
@@ -37,35 +37,35 @@ const Feed = () => {
     fetchPosts();
   }, []);
 
-  const filterPrompts = (searchText) => {
-    const regex = new RegExp(searchText, "i");
-    return posts.filter(
-      (item) =>
-      regex.test(item.creator.username) ||
-      regex.test(item.tag) ||
-      regex.test(item.prompt)
-    );
-  }
+  // const filterPrompts = (searchText) => {
+  //   const regex = new RegExp(searchText, "i");
+  //   return posts.filter(
+  //     (item) =>
+  //     regex.test(item.creator.username) ||
+  //     regex.test(item.tag) ||
+  //     regex.test(item.prompt)
+  //   );
+  // }
 
-  const handleSearchChange = (e) => {
-    clearTimeout(searchTimeout);
-    setSearchText(e.target.value);
+  // const handleSearchChange = (e) => {
+  //   clearTimeout(searchTimeout);
+  //   setSearchText(e.target.value);
 
-    //debounce method
-    setSearchTimeout(
-      setTimeout(() => {
-      const searchResult = filterPrompts(e.target.value);
-      setSearchedResults(searchResult);
-    }, 500)
-    );
-  };
+  //   //debounce method
+  //   setSearchTimeout(
+  //     setTimeout(() => {
+  //     const searchResult = filterPrompts(e.target.value);
+  //     setSearchedResults(searchResult);
+  //   }, 500)
+  //   );
+  // };
 
-  const handleTagClick = (tagName) => {
-    setSearchText(tagName);
+  // const handleTagClick = (tagName) => {
+  //   setSearchText(tagName);
 
-    const searchResult = filterPrompts(tagName);
-    setSearchedResults(searchResult);
-  };
+  //   const searchResult = filterPrompts(tagName);
+  //   setSearchedResults(searchResult);
+  // };
   
 
   return (
@@ -75,7 +75,7 @@ const Feed = () => {
         type='text'
         placeholder='Search for a tag or a username'
         value={searchText}
-        onChange={handleSearchChange}
+        onChange={() => {}}
         required
         className='search_input peer'/>
       </form>
